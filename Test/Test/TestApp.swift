514c5d4db0a28e3b3587662df4edf7d35d9b3d87
guilhermebarth <40143277+guilhermebarth@users.noreply.github.com>
2022-09-15 21:17:26 -0300

    Update

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,22 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Guilherme on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+        let persistenceController = PersistenceController.shared
+        
+        var body: some Scene {
+            WindowGroup {
+                SwiftUIView_Previews.previews
+                
+            }
+        }
+    
+    
+    }
